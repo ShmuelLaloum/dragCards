0dc2ac3fee66f4dd2b4e933dd47f7863c1bb5d22
ShmuelLaloum <shmuel826@gmail.com>
2026-01-07 11:28:23 +0200

    עדכון החזרתיות של הקוד

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,11 +6,12 @@ import ErrorState from "../components/ErrorState";
 import SearchSection from "../components/SearchSection";
 import { DndContext } from "@dnd-kit/core";
 import { useKanbanBoard } from "../hooks/useKanbanBoard";
+import type { ColumnType } from "../types/type";
 
 export default function Dashboard() {
   const { cardsState, handleDragEnd, sensors, isLoading, error, refetch } =
     useKanbanBoard();
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
   if (isLoading) return <Loader />;
   if (error) return <ErrorState onRetry={refetch} />;
@@ -19,35 +20,39 @@ export default function Dashboard() {
     item.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const columns: {
+    id: ColumnType;
+    title: string;
+    items: typeof filteredAvailable;
+  }[] = [
+    {
+      id: "available",
+      title: `🟦 Available (${filteredAvailable.length})`,
+      items: filteredAvailable,
+    },
+    {
+      id: "selected",
+      title: `🟨 Selected (${cardsState.selected.length})`,
+      items: cardsState.selected,
+    },
+    {
+      id: "favorites",
+      title: `🟩 Favorites (${cardsState.favorites.length})`,
+      items: cardsState.favorites,
+    },
+  ];
+
   return (
     <div className="dashboard-container">
       <SearchSection onSearch={setSearchTerm} />
 
       <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
         <Row gutter={[16, 16]}>
-          <Col xs={24} md={8}>
-            <Column
-              id="available"
-              title={`🟦 Available (${filteredAvailable.length})`}
-              items={filteredAvailable}
-            />
-          </Col>
-
-          <Col xs={24} md={8}>
-            <Column
-              id="selected"
-              title={`🟨 Selected (${cardsState.selected.length})`}
-              items={cardsState.selected}
-            />
-          </Col>
-
-          <Col xs={24} md={8}>
-            <Column
-              id="favorites"
-              title={`🟩 Favorites (${cardsState.favorites.length})`}
-              items={cardsState.favorites}
-            />
-          </Col>
+          {columns.map((col) => (
+            <Col xs={24} md={8} key={col.id}>
+              <Column id={col.id} title={col.title} items={col.items} />
+            </Col>
+          ))}
         </Row>
       </DndContext>
     </div>
